d39292bbb4b3008df23d4ed4674124f97958bfc0
Benjamin Abbott <ben_abbott@outlook.com>
2025-05-12 18:26:10 +0100

    testing api calls and formatting response

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -1,13 +1,26 @@
 import pandas as pd
-# import requests
+import requests
+# from pathlib import Path
+import os
 from etl.transform.transform_tickers import clean_nyse_tickers
 from etl.transform.transform_tickers import clean_nasdaq_tickers
 from etl.transform.transform_tickers import join_tickers
 
+API_KEY = 'b95993f82e2d0048935cfd947df28088'
+NASDAQ_FILE_PATH = os.path.join(
+    os.path.dirname(__file__), '../../data/raw/nasdaq_tickers.txt'
+)
+NYSE_FILE_PATH = os.path.join(
+    os.path.dirname(__file__), '../../data/raw/other_tickers.txt'
+)
+
+# NASDAQ_FILE_PATH = 'data/raw/nasdaq_tickers.txt'
+# NYSE_FILE_PATH = 'data/raw/other_tickers.txt'
+
 
 def get_ticker_lists() -> list:
-    nasdaq_tickers = pd.read_csv('data/raw/nasdaq_tickers.txt', sep='|')
-    nyse_tickers = pd.read_csv('data/raw/other_tickers.txt', sep='|')
+    nasdaq_tickers = pd.read_csv(NASDAQ_FILE_PATH, sep='|')
+    nyse_tickers = pd.read_csv(NYSE_FILE_PATH, sep='|')
     tickers_list = [nasdaq_tickers, nyse_tickers]
     return tickers_list
 
@@ -19,3 +32,19 @@ def extract_tickers():
     cleaned_nyse_tickers = clean_nyse_tickers(nyse_tickers)
     all_tickers_clean = join_tickers(cleaned_nas_tickers, cleaned_nyse_tickers)
     return all_tickers_clean
+
+
+# Get fundamental data for calculations
+
+def extract_from_balance_sheet(tickers):
+    pass
+
+# Get stock info data for insights
+
+
+def extract_from_company_info(tickers):
+    pass
+
+
+def extract_from_financial_score(tickers):
+    pass
