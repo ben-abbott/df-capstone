f8a245756b0f5b20a89bc3b98c108393b15ae7c5
Benjamin Abbott <ben_abbott@outlook.com>
2025-05-14 11:44:47 +0100

    uploads to s3 bucket and fixed env variables

--- scripts/run_etl.py
+++ scripts/run_etl.py
@@ -4,6 +4,7 @@ from config.env_config import setup_env
 from etl.extract.extract import extract_tickers
 from etl.extract.extract import extract_data
 from etl.transform.transform import transform_data
+from etl.load.load import load_to_bucket
 
 
 def main():
@@ -13,12 +14,13 @@ def main():
         f"ETL pipeline run successfully in "
         f'{os.getenv("ENV", "error")} environment!'
     )
+    clean_data_file_path = 'data/clean/clean_ranked_data.csv'
+    clean_data_file = 'clean_ranked_data.csv'
     ticker_df = extract_tickers()
-    # tickers_list = ticker_df['symbol'].tolist()
     all_api_data = extract_data(ticker_df)
-    # print(ticker_df)
     transformed_data = transform_data(all_api_data)
-    transformed_data.to_csv('data/clean/clean_ranked_data4.csv', index=False)
+    transformed_data.to_csv(clean_data_file_path, index=False)
+    load_to_bucket(clean_data_file_path, clean_data_file)
 
 
 def run_env_setup():
